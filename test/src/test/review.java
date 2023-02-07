1ca2818dcc9650ded7f059961632b101fcd8b5df
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2023-02-07 13:48:17 +0900

    Add files via upload

---
+++ test/src/test/review.java
@@ -0,0 +1,62 @@
+package test;
+
+public class review {
+	
+	/* 객체 
+	 *	- 객체 생성시 heap 메로리 공간에 할당. 
+	 *	studnet s = new studnet(); 
+	 *	s = 객체가 아닌 객체의 주소값이 들어있음
+	 * 
+	 * 상속 
+	 * - 클래스의 재사용, 다형성 적용
+	 * - 모든 클래스는 object의 후손
+	 * - 부모의 생성자 super()로 접근, private 접근불가 set/get 이용
+	 * 
+	 * 오버라이딩
+	 * - 부모클래스 -> 자식클래스 재정의 (이름, 매개변수개수,타입 , 리턴타입 동일)
+	 * - privete/ final 오버라이딩 불가
+	 * 
+	 * 오버로딩 
+	 * - 한 클래스내에서 같은 이름의 메소드를 여러개 정의 (같은 이름, 다른매개변수(타입,개수, 순서)
+	 * 
+	 * 다형성
+	 * - 많은 자료형 - 하나의 객체가 많은 자료형을 가질 수 있음.
+	 * - 상속을 이용해 부모탑으로 파생된 여러가지 타입을 자식 클래스하나로 사용
+	 * 
+	 * 인터페이스 
+	 * - 상수형필드/추상메소드만 작성 public abstract 고정 (생략가능)
+	 * - 상속시 오버라이딩해야함 
+	 * - 객체생성은 안되나 참조형 변수로는 가능(다형성 이용)
+	 * 
+	 * 입출력 IO
+	 * 
+	 * 
+	 * 자바 컬렉션
+	 * - hasmap - 객체와 비슷 
+	 * - praperties = mpa 계열 / key,value 세트로 저장(key=vlaue)로 저장 
+	 * - arraylist - 배열 
+	 * 
+	 * load("test.properties") - > 일반 prop 읽어오기
+	 * prop loadFromXML(new FileInputStram("test1.xml")); -> XML 읽어오기
+	 * 
+	 * 네트워크
+	 * - 소켓프로그래밍. 서 - 클 통신이 가능
+	 * 
+	 * arrayList 
+	 * - 모든 타입 받을 수 있음. 
+	 * 
+	 * lamda 식 
+	 * 변수 변수명 = (매개변수) -> {구현코드};
+	 * 
+	 * stream 
+	 * staeam을 사용하기 위해서는 스트림 화 시켜야함. Arrays.stram(foods).filer(food -> food.charAt(0) == '마')
+	 * 
+	 * 
+	 * */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
